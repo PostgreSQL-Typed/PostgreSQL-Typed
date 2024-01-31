6aa587064600e8d2c9a7ded3d09a1b2489d46f8b
Bas van Zanten <me@bas950.com>
2024-01-31 12:26:02 +0100

    fix(driver): setTypeParser per client not globally (#133)

--- packages/core/src/driver.ts
+++ packages/core/src/driver.ts
@@ -68,130 +68,132 @@ export class PgTDriver {
 	}
 
 	initMappers(): void {
-		types.setTypeParser(OID.bytea as any, defaultParserMappings[OID.bytea].parse);
-		types.setTypeParser(OID._bytea as any, defaultParserMappings[OID._bytea].parse);
+		//TODO: Update the @types/pg package to include the function
+		const client = this.client as pg.Client & { setTypeParser: typeof types.setTypeParser };
+		client.setTypeParser(OID.bytea as any, defaultParserMappings[OID.bytea].parse);
+		client.setTypeParser(OID._bytea as any, defaultParserMappings[OID._bytea].parse);
 
-		types.setTypeParser(OID.bit as any, defaultParserMappings[OID.bit].parse);
-		types.setTypeParser(OID._bit as any, defaultParserMappings[OID._bit].parse);
+		client.setTypeParser(OID.bit as any, defaultParserMappings[OID.bit].parse);
+		client.setTypeParser(OID._bit as any, defaultParserMappings[OID._bit].parse);
 
-		types.setTypeParser(OID.varbit as any, defaultParserMappings[OID.varbit].parse);
-		types.setTypeParser(OID._varbit as any, defaultParserMappings[OID._varbit].parse);
+		client.setTypeParser(OID.varbit as any, defaultParserMappings[OID.varbit].parse);
+		client.setTypeParser(OID._varbit as any, defaultParserMappings[OID._varbit].parse);
 
-		types.setTypeParser(OID.bool as any, defaultParserMappings[OID.bool].parse);
-		types.setTypeParser(OID._bool as any, defaultParserMappings[OID._bool].parse);
+		client.setTypeParser(OID.bool as any, defaultParserMappings[OID.bool].parse);
+		client.setTypeParser(OID._bool as any, defaultParserMappings[OID._bool].parse);
 
-		types.setTypeParser(OID.char as any, defaultParserMappings[OID.char].parse);
-		types.setTypeParser(OID.bpchar as any, defaultParserMappings[OID.bpchar].parse);
-		types.setTypeParser(OID._char as any, defaultParserMappings[OID._char].parse);
-		types.setTypeParser(OID._bpchar as any, defaultParserMappings[OID._bpchar].parse);
+		client.setTypeParser(OID.char as any, defaultParserMappings[OID.char].parse);
+		client.setTypeParser(OID.bpchar as any, defaultParserMappings[OID.bpchar].parse);
+		client.setTypeParser(OID._char as any, defaultParserMappings[OID._char].parse);
+		client.setTypeParser(OID._bpchar as any, defaultParserMappings[OID._bpchar].parse);
 
-		types.setTypeParser(OID.varchar as any, defaultParserMappings[OID.varchar].parse);
-		types.setTypeParser(OID._varchar as any, defaultParserMappings[OID._varchar].parse);
+		client.setTypeParser(OID.varchar as any, defaultParserMappings[OID.varchar].parse);
+		client.setTypeParser(OID._varchar as any, defaultParserMappings[OID._varchar].parse);
 
-		types.setTypeParser(OID.name as any, defaultParserMappings[OID.name].parse);
-		types.setTypeParser(OID._name as any, defaultParserMappings[OID._name].parse);
+		client.setTypeParser(OID.name as any, defaultParserMappings[OID.name].parse);
+		client.setTypeParser(OID._name as any, defaultParserMappings[OID._name].parse);
 
-		types.setTypeParser(OID.text as any, defaultParserMappings[OID.text].parse);
-		types.setTypeParser(OID._text as any, defaultParserMappings[OID._text].parse);
+		client.setTypeParser(OID.text as any, defaultParserMappings[OID.text].parse);
+		client.setTypeParser(OID._text as any, defaultParserMappings[OID._text].parse);
 
-		types.setTypeParser(OID.date as any, defaultParserMappings[OID.date].parse);
-		types.setTypeParser(OID._date as any, defaultParserMappings[OID._date].parse);
+		client.setTypeParser(OID.date as any, defaultParserMappings[OID.date].parse);
+		client.setTypeParser(OID._date as any, defaultParserMappings[OID._date].parse);
 
-		types.setTypeParser(OID.datemultirange as any, defaultParserMappings[OID.datemultirange].parse);
-		types.setTypeParser(OID._datemultirange as any, defaultParserMappings[OID._datemultirange].parse);
+		client.setTypeParser(OID.datemultirange as any, defaultParserMappings[OID.datemultirange].parse);
+		client.setTypeParser(OID._datemultirange as any, defaultParserMappings[OID._datemultirange].parse);
 
-		types.setTypeParser(OID.daterange as any, defaultParserMappings[OID.daterange].parse);
-		types.setTypeParser(OID._daterange as any, defaultParserMappings[OID._daterange].parse);
+		client.setTypeParser(OID.daterange as any, defaultParserMappings[OID.daterange].parse);
+		client.setTypeParser(OID._daterange as any, defaultParserMappings[OID._daterange].parse);
 
-		types.setTypeParser(OID.interval as any, defaultParserMappings[OID.interval].parse);
-		types.setTypeParser(OID._interval as any, defaultParserMappings[OID._interval].parse);
+		client.setTypeParser(OID.interval as any, defaultParserMappings[OID.interval].parse);
+		client.setTypeParser(OID._interval as any, defaultParserMappings[OID._interval].parse);
 
-		types.setTypeParser(OID.time as any, defaultParserMappings[OID.time].parse);
-		types.setTypeParser(OID._time as any, defaultParserMappings[OID._time].parse);
+		client.setTypeParser(OID.time as any, defaultParserMappings[OID.time].parse);
+		client.setTypeParser(OID._time as any, defaultParserMappings[OID._time].parse);
 
-		types.setTypeParser(OID.timestamp as any, defaultParserMappings[OID.timestamp].parse);
-		types.setTypeParser(OID._timestamp as any, defaultParserMappings[OID._timestamp].parse);
+		client.setTypeParser(OID.timestamp as any, defaultParserMappings[OID.timestamp].parse);
+		client.setTypeParser(OID._timestamp as any, defaultParserMappings[OID._timestamp].parse);
 
-		types.setTypeParser(OID.tsmultirange as any, defaultParserMappings[OID.tsmultirange].parse);
-		types.setTypeParser(OID._tsmultirange as any, defaultParserMappings[OID._tsmultirange].parse);
+		client.setTypeParser(OID.tsmultirange as any, defaultParserMappings[OID.tsmultirange].parse);
+		client.setTypeParser(OID._tsmultirange as any, defaultParserMappings[OID._tsmultirange].parse);
 
-		types.setTypeParser(OID.tsrange as any, defaultParserMappings[OID.tsrange].parse);
-		types.setTypeParser(OID._tsrange as any, defaultParserMappings[OID._tsrange].parse);
+		client.setTypeParser(OID.tsrange as any, defaultParserMappings[OID.tsrange].parse);
+		client.setTypeParser(OID._tsrange as any, defaultParserMappings[OID._tsrange].parse);
 
-		types.setTypeParser(OID.timestamptz as any, defaultParserMappings[OID.timestamptz].parse);
-		types.setTypeParser(OID._timestamptz as any, defaultParserMappings[OID._timestamptz].parse);
+		client.setTypeParser(OID.timestamptz as any, defaultParserMappings[OID.timestamptz].parse);
+		client.setTypeParser(OID._timestamptz as any, defaultParserMappings[OID._timestamptz].parse);
 
-		types.setTypeParser(OID.tstzmultirange as any, defaultParserMappings[OID.tstzmultirange].parse);
-		types.setTypeParser(OID._tstzmultirange as any, defaultParserMappings[OID._tstzmultirange].parse);
+		client.setTypeParser(OID.tstzmultirange as any, defaultParserMappings[OID.tstzmultirange].parse);
+		client.setTypeParser(OID._tstzmultirange as any, defaultParserMappings[OID._tstzmultirange].parse);
 
-		types.setTypeParser(OID.tstzrange as any, defaultParserMappings[OID.tstzrange].parse);
-		types.setTypeParser(OID._tstzrange as any, defaultParserMappings[OID._tstzrange].parse);
+		client.setTypeParser(OID.tstzrange as any, defaultParserMappings[OID.tstzrange].parse);
+		client.setTypeParser(OID._tstzrange as any, defaultParserMappings[OID._tstzrange].parse);
 
-		types.setTypeParser(OID.timetz as any, defaultParserMappings[OID.timetz].parse);
-		types.setTypeParser(OID._timetz as any, defaultParserMappings[OID._timetz].parse);
+		client.setTypeParser(OID.timetz as any, defaultParserMappings[OID.timetz].parse);
+		client.setTypeParser(OID._timetz as any, defaultParserMappings[OID._timetz].parse);
 
-		types.setTypeParser(OID.box as any, defaultParserMappings[OID.box].parse);
-		types.setTypeParser(OID._box as any, defaultParserMappings[OID._box].parse);
+		client.setTypeParser(OID.box as any, defaultParserMappings[OID.box].parse);
+		client.setTypeParser(OID._box as any, defaultParserMappings[OID._box].parse);
 
-		types.setTypeParser(OID.circle as any, defaultParserMappings[OID.circle].parse);
-		types.setTypeParser(OID._circle as any, defaultParserMappings[OID._circle].parse);
+		client.setTypeParser(OID.circle as any, defaultParserMappings[OID.circle].parse);
+		client.setTypeParser(OID._circle as any, defaultParserMappings[OID._circle].parse);
 
-		types.setTypeParser(OID.line as any, defaultParserMappings[OID.line].parse);
-		types.setTypeParser(OID._line as any, defaultParserMappings[OID._line].parse);
+		client.setTypeParser(OID.line as any, defaultParserMappings[OID.line].parse);
+		client.setTypeParser(OID._line as any, defaultParserMappings[OID._line].parse);
 
-		types.setTypeParser(OID.lseg as any, defaultParserMappings[OID.lseg].parse);
-		types.setTypeParser(OID._lseg as any, defaultParserMappings[OID._lseg].parse);
+		client.setTypeParser(OID.lseg as any, defaultParserMappings[OID.lseg].parse);
+		client.setTypeParser(OID._lseg as any, defaultParserMappings[OID._lseg].parse);
 
-		types.setTypeParser(OID.path as any, defaultParserMappings[OID.path].parse);
-		types.setTypeParser(OID._path as any, defaultParserMappings[OID._path].parse);
+		client.setTypeParser(OID.path as any, defaultParserMappings[OID.path].parse);
+		client.setTypeParser(OID._path as any, defaultParserMappings[OID._path].parse);
 
-		types.setTypeParser(OID.point as any, defaultParserMappings[OID.point].parse);
-		types.setTypeParser(OID._point as any, defaultParserMappings[OID._point].parse);
+		client.setTypeParser(OID.point as any, defaultParserMappings[OID.point].parse);
+		client.setTypeParser(OID._point as any, defaultParserMappings[OID._point].parse);
 
-		types.setTypeParser(OID.polygon as any, defaultParserMappings[OID.polygon].parse);
-		types.setTypeParser(OID._polygon as any, defaultParserMappings[OID._polygon].parse);
+		client.setTypeParser(OID.polygon as any, defaultParserMappings[OID.polygon].parse);
+		client.setTypeParser(OID._polygon as any, defaultParserMappings[OID._polygon].parse);
 
-		types.setTypeParser(OID.json as any, defaultParserMappings[OID.json].parse);
-		types.setTypeParser(OID._json as any, defaultParserMappings[OID._json].parse);
+		client.setTypeParser(OID.json as any, defaultParserMappings[OID.json].parse);
+		client.setTypeParser(OID._json as any, defaultParserMappings[OID._json].parse);
 
-		types.setTypeParser(OID.jsonb as any, defaultParserMappings[OID.jsonb].parse);
-		types.setTypeParser(OID._jsonb as any, defaultParserMappings[OID._jsonb].parse);
+		client.setTypeParser(OID.jsonb as any, defaultParserMappings[OID.jsonb].parse);
+		client.setTypeParser(OID._jsonb as any, defaultParserMappings[OID._jsonb].parse);
 
-		types.setTypeParser(OID.money as any, defaultParserMappings[OID.money].parse);
-		types.setTypeParser(OID._money as any, defaultParserMappings[OID._money].parse);
+		client.setTypeParser(OID.money as any, defaultParserMappings[OID.money].parse);
+		client.setTypeParser(OID._money as any, defaultParserMappings[OID._money].parse);
 
-		types.setTypeParser(OID.float4 as any, defaultParserMappings[OID.float4].parse);
-		types.setTypeParser(OID._float4 as any, defaultParserMappings[OID._float4].parse);
+		client.setTypeParser(OID.float4 as any, defaultParserMappings[OID.float4].parse);
+		client.setTypeParser(OID._float4 as any, defaultParserMappings[OID._float4].parse);
 
-		types.setTypeParser(OID.float8 as any, defaultParserMappings[OID.float8].parse);
-		types.setTypeParser(OID._float8 as any, defaultParserMappings[OID._float8].parse);
+		client.setTypeParser(OID.float8 as any, defaultParserMappings[OID.float8].parse);
+		client.setTypeParser(OID._float8 as any, defaultParserMappings[OID._float8].parse);
 
-		types.setTypeParser(OID.int2 as any, defaultParserMappings[OID.int2].parse);
-		types.setTypeParser(OID._int2 as any, defaultParserMappings[OID._int2].parse);
+		client.setTypeParser(OID.int2 as any, defaultParserMappings[OID.int2].parse);
+		client.setTypeParser(OID._int2 as any, defaultParserMappings[OID._int2].parse);
 
-		types.setTypeParser(OID.int4 as any, defaultParserMappings[OID.int4].parse);
-		types.setTypeParser(OID._int4 as any, defaultParserMappings[OID._int4].parse);
+		client.setTypeParser(OID.int4 as any, defaultParserMappings[OID.int4].parse);
+		client.setTypeParser(OID._int4 as any, defaultParserMappings[OID._int4].parse);
 
-		types.setTypeParser(OID.int4multirange as any, defaultParserMappings[OID.int4multirange].parse);
-		types.setTypeParser(OID._int4multirange as any, defaultParserMappings[OID._int4multirange].parse);
+		client.setTypeParser(OID.int4multirange as any, defaultParserMappings[OID.int4multirange].parse);
+		client.setTypeParser(OID._int4multirange as any, defaultParserMappings[OID._int4multirange].parse);
 
-		types.setTypeParser(OID.int4range as any, defaultParserMappings[OID.int4range].parse);
-		types.setTypeParser(OID._int4range as any, defaultParserMappings[OID._int4range].parse);
+		client.setTypeParser(OID.int4range as any, defaultParserMappings[OID.int4range].parse);
+		client.setTypeParser(OID._int4range as any, defaultParserMappings[OID._int4range].parse);
 
-		types.setTypeParser(OID.int8 as any, defaultParserMappings[OID.int8].parse);
-		types.setTypeParser(OID._int8 as any, defaultParserMappings[OID._int8].parse);
+		client.setTypeParser(OID.int8 as any, defaultParserMappings[OID.int8].parse);
+		client.setTypeParser(OID._int8 as any, defaultParserMappings[OID._int8].parse);
 
-		types.setTypeParser(OID.int8multirange as any, defaultParserMappings[OID.int8multirange].parse);
-		types.setTypeParser(OID._int8multirange as any, defaultParserMappings[OID._int8multirange].parse);
+		client.setTypeParser(OID.int8multirange as any, defaultParserMappings[OID.int8multirange].parse);
+		client.setTypeParser(OID._int8multirange as any, defaultParserMappings[OID._int8multirange].parse);
 
-		types.setTypeParser(OID.int8range as any, defaultParserMappings[OID.int8range].parse);
-		types.setTypeParser(OID._int8range as any, defaultParserMappings[OID._int8range].parse);
+		client.setTypeParser(OID.int8range as any, defaultParserMappings[OID.int8range].parse);
+		client.setTypeParser(OID._int8range as any, defaultParserMappings[OID._int8range].parse);
 
-		types.setTypeParser(OID.oid as any, defaultParserMappings[OID.oid].parse);
-		types.setTypeParser(OID._oid as any, defaultParserMappings[OID._oid].parse);
+		client.setTypeParser(OID.oid as any, defaultParserMappings[OID.oid].parse);
+		client.setTypeParser(OID._oid as any, defaultParserMappings[OID._oid].parse);
 
-		types.setTypeParser(OID.uuid as any, defaultParserMappings[OID.uuid].parse);
-		types.setTypeParser(OID._uuid as any, defaultParserMappings[OID._uuid].parse);
+		client.setTypeParser(OID.uuid as any, defaultParserMappings[OID.uuid].parse);
+		client.setTypeParser(OID._uuid as any, defaultParserMappings[OID._uuid].parse);
 	}
 }
 
